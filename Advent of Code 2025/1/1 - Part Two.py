94bd476d1d7a3d162964be91ba45692c5ea75a90
Jergus Golian <jergus.golian@gmail.com>
2025-12-01 14:37:26 +0100

    Add files via upload

---
+++ Advent of Code 2025/1/1 - Part Two.py
@@ -0,0 +1,24 @@
+text = open("1/input.txt", "r")
+instructions = text.readlines()
+current_number = 50
+zeros = 0
+
+print(f"  - The dial starts by pointing at {current_number}.")
+
+for i in range(len(instructions)):
+    line = instructions[i].strip()
+    direction = line[0]
+    number = int(line[1:])
+
+    if direction == 'R':
+        current_number = (current_number + number) % 100
+    elif direction == 'L':
+        current_number = (current_number - number + 100) % 100
+    if current_number == 0:
+        zeros += 1
+
+    print(f"  - The dial is rotated {line} to point at {current_number}.")
+
+print()
+print(f"Final count of zeros is {zeros}")
+print()
